129188875ed29e74673446eb33d7ac519d708537
levyc <491796258@qq.com>
2018-11-09 01:04:08 +0800

    update

--- event/src/main/java/com/snower/event/EventManager.java
+++ event/src/main/java/com/snower/event/EventManager.java
@@ -1,14 +1,40 @@
 package com.snower.event;
 
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import com.snower.event.annotation.EventHandler;
+/**
+ * 事件管理器
+ * @author levy
+ *
+ */
 public class EventManager {
 	
 	/**各种事件订阅者集合K:事件类型  V:订阅者集合*/
 	private Map<Integer,Set<EventSubscriber>> subscribers = new HashMap<Integer, Set<EventSubscriber>>();
 	
-	
+	/**
+	 * 注册事件订阅者
+	 * @param target
+	 */
+	public void registerSubscriber(Object target){
+		
+		Class<? extends Object> class1 = target.getClass();
+		Method[] methods = class1.getMethods();
+		for(Method method:methods){
+			method.setAccessible(true);
+			EventHandler annotation = method.getAnnotation(EventHandler.class);
+			if(annotation==null){
+				continue;
+			}
+			EventSubscriber subscriber = new EventSubscriber(target,method);
+			int eventType = annotation.value();
+			Set<EventSubscriber> set = subscribers.get(eventType);
+		}
+		
+	}
 	
 }
---
+++ event/src/main/java/com/snower/event/annotation/Listener.java
@@ -0,0 +1,20 @@
+package com.snower.event.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+/**
+ * 包含事件处理器的监听器类注解
+ * @author levy
+ *
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface Listener {
+	
+}
